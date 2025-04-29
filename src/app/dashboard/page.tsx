170310bebd69b491b32f92fa9607c2135fd8e178
r-mccarty <ry.mccarty13@gmail.com>
2025-04-28 20:58:40 -0700

    initialized template, removed tree component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+'use client';  // Important for client-side components with interactivity
+
+import Dashboard from '../../../dashboard/Dashboard';
+
+export default function DashboardPage() {
+  return <Dashboard />;
+}
